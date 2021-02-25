9340e2640d2f3c9513f77dca168d264095cbff3b
Lian Shao <lianshao2000@gmail.com>
2021-02-25 03:34:18 -0800

    refactored the json string processing part of addDataset, removed the eslint warning comments.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -70,10 +70,35 @@ export default class InsightFacade implements IInsightFacade {
         return true;
     }
 
+    public processJSONString(courseJSONString: string) {
+        let object = JSON.parse(courseJSONString);
+        if (!object.hasOwnProperty("result")) {
+            throw new InsightError("Results key does not exist within this json");
+        }
+        if (object.result.length !== 0) {
+            for (const i of object.result) {
+                if (!this.testJSONHasRequiredProperties(i)) {
+                    throw new InsightError("Invalid JSON file formats");
+                }
+                const section: SectionObject = {
+                    dept: i.Subject,
+                    id: i.Course,
+                    avg: i.Avg,
+                    instructor: i.Professor,
+                    title: i.Title,
+                    pass: i.Pass,
+                    fail: i.Fail,
+                    audit: i.Audit,
+                    uuid: i.id,
+                    year: i.Year
+                };
+                this.listOfSections.push(section);
+                this.numRows += 1;
+            }
+        }
+    }
 
-    // eslint-disable-next-line @typescript-eslint/tslint/config
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-
         // every file is {"result": [{}]} if allEmpty = true
         let allEmpty: boolean = true;
         let coursePromisesArray: Array<Promise<string>> = [];
@@ -82,12 +107,9 @@ export default class InsightFacade implements IInsightFacade {
 
         let zip = new JSZip();
 
-        // eslint-disable-next-line @typescript-eslint/tslint/config
         return new Promise<string[]>((resolve, reject) => {
             let idTestRet = this.idTestHelper(id, "add", kind);
-            if (idTestRet !== null) {
-                return reject(idTestRet);
-            }
+            if (idTestRet !== null) { return reject(idTestRet); }
             // z = unzipped jszip object
             return Promise.all([zip.loadAsync(content, { base64: true })]).then((z: JSZip[]) => {
                 z[0].folder("courses").forEach(function (relativePath: string, file: JSZip.JSZipObject) {
@@ -95,60 +117,27 @@ export default class InsightFacade implements IInsightFacade {
                     coursePromisesArray.push(file.async("text"));
                 });
                 return Promise.all(coursePromisesArray);
-                // eslint-disable-next-line @typescript-eslint/tslint/config
             }).then((resolvedCourses: string[]) => {
-                if (!resolvedCourses.length) {
-                    return reject(new InsightError("empty"));
-                }
+                if (!resolvedCourses.length) { return reject(new InsightError("empty")); }
                 const detailedDataset: DetailedDataset = {
-                    id: id,
-                    data: [],
-                    kind: kind
+                    id: id, data: [], kind: kind
                 };
                 for (const courseJSONString of resolvedCourses) {
                     if (courseJSONString) {
                         try {
-                            let object = JSON.parse(courseJSONString);
-                            if (!object.hasOwnProperty("result")) {
-                                throw new InsightError("Results key does not exist within this json");
-                            }
-                            if (object.result.length !== 0) {
-                                for (const i of object.result) {
-                                    if (!this.testJSONHasRequiredProperties(i)) {
-                                        return reject(new InsightError("Invalid JSON file formats"));
-                                    }
-                                    const section: SectionObject = {
-                                        dept: i.Subject,
-                                        id: i.Course,
-                                        avg: i.Avg,
-                                        instructor: i.Professor,
-                                        title: i.Title,
-                                        pass: i.Pass,
-                                        fail: i.Fail,
-                                        audit: i.Audit,
-                                        uuid: i.id,
-                                        year: i.Year
-                                    };
-                                    this.listOfSections.push(section);
-                                    // detailedDataset.data.push(section);
-                                    this.numRows += 1;
-                                }
-                                detailedDataset.data = this.listOfSections;
-                                allEmpty = false;
-                            }
+                            this.processJSONString(courseJSONString);
+                            allEmpty = false;
                         } catch (err) {
-                            Log.trace(err);
                             return reject(new InsightError(err));
                         }
                     }
                 }
+                detailedDataset.data = this.listOfSections;
                 if (allEmpty) { return reject(new InsightError("zip contains only empty jsons")); }
                 fs.writeFileSync("data/" + id + ".json", JSON.stringify(detailedDataset));
 
                 const retDataset: InsightDataset = {
-                    id: id,
-                    kind: InsightDatasetKind.Courses,
-                    numRows: this.numRows
+                    id: id, kind: InsightDatasetKind.Courses, numRows: this.numRows
                 };
 
                 this.listOfDatasets.push(retDataset);
@@ -181,7 +170,7 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public performQuery(query: any): Promise<any[]> {
-        // return Promise.reject("Not implemented.");
+        return Promise.reject("Not implemented.");
         let p = new PerformQuery();
         return p.performQuery(query);
     }
