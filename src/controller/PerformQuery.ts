966c834b725d31022299853a0495bd009c69a649
Regina Wong <reginawongmm@gmail.com>
2021-02-25 17:22:44 -0800

    Updated perform query to handle the filters better

--- src/controller/PerformQuery.ts
+++ src/controller/PerformQuery.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/tslint/config */
 import JSZip = require("jszip");
 import * as fs from "fs-extra";
 import Log from "../Util";
@@ -11,25 +10,25 @@ let resultArr: string[] = [];
 let filters: string[] = ["AND", "OR", "LT", "GT", "EQ", "IS", "NOT"];
 
 export default class PerformQuery {
-    public handleM(mkey: string, numVal: number, data: any): boolean {
+    public handleM(mkey: string, numVal: number, jsonDataSingle: any): boolean {
         switch (mkey) {
             case "LT": {
-                if (data.data.mkey < numVal) {
-                    resultArr.push(data.data.filterKeyVal.value);
+                if (jsonDataSingle.mkey < numVal) {
+                    resultArr.push(jsonDataSingle.data);
                     return true;
                 }
                 break;
             }
             case "GT": {
-                if (data.data.mkey > numVal) {
-                    resultArr.push(data.data.filterKeyVal.value);
+                if (jsonDataSingle.mkey > numVal) {
+                    resultArr.push(jsonDataSingle.data);
                     return true;
                 }
                 break;
             }
             case "EQ": {
-                if (data.data.mkey === numVal) {
-                    resultArr.push(data.data.filterKeyVal.value);
+                if (jsonDataSingle.mkey === numVal) {
+                    resultArr.push(jsonDataSingle.data);
                     return true;
                 }
                 break;
@@ -37,13 +36,50 @@ export default class PerformQuery {
         }
         return false;
     }
+    public handleS(sfield: string, data: any): boolean {
+        if (data.data.IS === sfield) {
+            resultArr.push(data.data);
+            return true;
+        }
+        return false;
+    }
+
+        // eslint-disable-next-line @typescript-eslint/tslint/config
+    public mCompareHandler(jsonObj: any, jsonData: any): boolean {
+        // mfieldArr = mfieldArr.map((x) => idstring.concat("_", x).join(""));
+        let key: any;
+        if (jsonObj.hasOwnProperty("LT")) {
+            key = jsonObj.LT;
+        } else if (jsonObj.hasOwnProperty("GT")) {
+            key = jsonObj.GT;
+        } else if (jsonObj.hasOwnProperty("EQ")) {
+            key = jsonObj.EQ;
+        }
+
+        if (filters.includes(jsonObj.LT)) {
+            return this.parseQuery(jsonObj.LT);
+        }
+        if (typeof jsonObj.get("LT") !== "number") {
+            throw new InsightError("Invalid value type");
+        }
+        if (mfieldArr.includes(jsonObj.LT)) {
+            for (const i in jsonData.data) {
+                return this.handleM(jsonObj.LT, jsonObj.get("LT"), i);
+            }
+        } else {
+            throw new InsightError("Invalid mkey");
+        }
+
+    }
+
+
+    // eslint-disable-next-line @typescript-eslint/tslint/config
     public parseQuery(jsonObj: any): boolean {
-        let matchInputStr: RegExp = /[^*]*/;
+        // let matchInputStr: RegExp = /[^*]*/;
         let wholeKey = jsonObj.OPTIONS.COLUMNS[1];
         let idstring = wholeKey.split("_", 1);
-        let data = JSON.parse(fs.readFileSync("data/" + idstring + ".json", "utf8"));
-        mfieldArr = mfieldArr.map((x) => idstring.concat("_", x).join(""));
-        sfieldArr = sfieldArr.map((x) => idstring.concat("_", x).join(""));
+        let jsonData = JSON.parse(fs.readFileSync("data/" + idstring + ".json", "utf8"));
+        // sfieldArr = sfieldArr.map((x) => idstring.concat("_", x).join(""));
         if (jsonObj.WHERE) {
             return this.parseQuery(jsonObj.WHERE);
         }
@@ -53,28 +89,27 @@ export default class PerformQuery {
         }
         // mcomparator
         if (jsonObj.LT || jsonObj.GT || jsonObj.EQ) {
-            let dataMKey = jsonObj.WHERE.filterVal;
-            let numVal = jsonObj.WHERE.filterVal.value;
-            if (numVal !== "number") {
-                throw new InsightError("Invalid value type");
-            }
-            if (mfieldArr.includes(dataMKey)) {
-                for (const i in data.data) {
-                    // !!!TODO jsonObj should instead be one of jsonObj.LT || jsonObj.GT || jsonObj.EQ
-                    return this.handleM(jsonObj, numVal, i);
-                }
-            } else {
-                throw new InsightError("Invalid mkey");
+            try {
+                this.mCompareHandler(jsonObj, jsonData);
+            } catch (error) {
+                throw new InsightError(error);
             }
         }
         // scomparators
         if (jsonObj.IS) {
+            // eslint-disable-next-line @typescript-eslint/tslint/config
+            if (filters.includes(jsonObj.IS)){
+                return this.parseQuery(jsonObj.IS);
+            }
             if (!sfieldArr.includes(jsonObj.IS)) {
                 throw new InsightError("Invalid skey");
             }
-            if (typeof jsonObj.WHERE.filterVal.value !== "string" ||
-                !matchInputStr.test(jsonObj.WHERE.filterVal.value)) {
+            if (typeof jsonObj.WHERE.GET("IS") !== "string") {
                 throw new InsightError("Invalid value type");
+            } else {
+                let sfieldConnected = jsonObj.IS;
+                let sfield = sfieldConnected.split("_", 2);
+                return this.handleS(sfield, jsonData.data);
             }
         }
         if (jsonObj.NOT) {
@@ -91,7 +126,7 @@ export default class PerformQuery {
             }
         }
         if (!jsonObj.OPTIONS.hasOwnProperty("COLUMNS")) {
-            throw new InsightError("Missing columns")
+            throw new InsightError("Missing columns");
         }
         if (jsonObj.OPTIONS.hasOwnProperty("ORDER")) {
             if (jsonObj.OPTIONS.ORDER === null) {
@@ -99,7 +134,7 @@ export default class PerformQuery {
             }
         }
         if (jsonObj.OPTIONS.COLUMNS.length === 0 || jsonObj.OPTIONS.COLUMNS.includes(null)) {
-            throw new InsightError("either columns is null or columns length is 0")
+            throw new InsightError("either columns is null or columns length is 0");
         }
         return true;
     }
@@ -108,7 +143,7 @@ export default class PerformQuery {
         return new Promise<any[]>((resolve, reject) => {
             try {
                 this.missingKeys(query);
-                this.parseQuery(query)
+                this.parseQuery(query);
             } catch (error) {
                 return reject(new InsightError(error));
             }
